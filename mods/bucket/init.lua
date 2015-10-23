20b2f42e0e9d4508bdf462697975c11a4963bfd5
narrnika <narr13niki@gmail.com>
2015-10-24 00:33:26 +0300

    add bucket with snow and ice

--- mods/bucket/init.lua
+++ mods/bucket/init.lua
@@ -184,4 +184,35 @@ minetest.register_craft({
 	replacements = {{"bucket:bucket_lava", "bucket:bucket_empty"}},
 })
 
+
+minetest.register_craftitem("bucket:bucket_ice", {
+	description = SL("Bucket With Ice"),
+	inventory_image = "bucket_ice.png",
+	stack_max = 1,
+})
+
+minetest.register_craft({output = 'bucket:bucket_ice 1',
+	recipe = {{'bucket:bucket_empty', 'default:ice'}}
+})
+
+minetest.register_craft({output = "bucket:bucket_water",
+	type = "cooking",
+	recipe = "bucket:bucket_ice",
+})
+
+minetest.register_craftitem("bucket:bucket_snow", {
+	description = SL("Bucket With Snow"),
+	inventory_image = "bucket_snow.png",
+	stack_max = 1,
+})
+
+minetest.register_craft({output = 'bucket:bucket_snow 1',
+	recipe = {{'bucket:bucket_empty', 'default:snowblock'}}
+})
+
+minetest.register_craft({output = "bucket:bucket_water",
+	type = "cooking",
+	recipe = "bucket:bucket_snow",
+})
+
 if minetest.setting_getbool("msg_loading_mods") then minetest.log("action", minetest.get_current_modname().." mod LOADED") end
