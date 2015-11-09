915f128899699d333e9799b78528d70100872f9c
narrnika <narr13niki@gmail.com>
2015-11-09 10:29:01 +0300

    add config 'sneak_glitch' for players

--- mods/default/player.lua
+++ mods/default/player.lua
@@ -147,6 +147,12 @@ minetest.register_on_joinplayer(function(player)
 
 	player:hud_set_hotbar_image("gui_hotbar.png")
 	player:hud_set_hotbar_selected_image("gui_hotbar_selected.png")
+
+	if minetest.setting_getbool("sneak_glitch") then
+		player:set_physics_override({sneak_glitch=true})
+	else
+		player:set_physics_override({sneak_glitch=false})
+	end
 end)
 
 minetest.register_on_leaveplayer(function(player)
